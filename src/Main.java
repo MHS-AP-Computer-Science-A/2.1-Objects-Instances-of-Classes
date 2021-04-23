bdf9608afd619daf87bc1817a67e7040dcaa5c00
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-04-23 10:17:27 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Instantiate an object
+		Rectangle r = new Rectangle(10, 5);
+		
+		//Use the object to compute the area
+		int area = r.length * r.width;
+		System.out.println("Area = " + area);
+	}
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+
+public class Rectangle {
+	//Instance Variables
+	//This is what all Rectangle Objects will have
+	int length;
+	int width;
+	
+	//Methods
+	//This is what all Rectangle Objects will do
+	
+	//Constructor to initialize instance variables
+	public Rectangle(int l, int w) {
+		length = l;
+		width = w;
+	}
+
+}
